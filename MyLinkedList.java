fe96ad295dc9fa87482a03b953093147e6270a6a
phuang00 <phuang00@stuy.edu>
2019-03-29 10:06:34 -0400

    added toString

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -4,6 +4,24 @@ public class MyLinkedList<E>{
 
   public MyLinkedList(){}
 
+  public String toString(){
+    String ans = "[";
+    // String ans equals open brackets
+    Node temp = start;
+    // temp is equal to start
+    while (temp != null){
+      // while temp is not null
+      ans += temp;
+      // toString of temp is added to ans
+      temp = temp.next();
+      // temp is equal to temp's next
+      if (temp != null) ans += ", ";
+      // if temp's next is not null, ", " is added to ans
+    }
+    ans += "]";
+    // ending bracket is added to ans
+    return ans;
+  }
 
   private class Node{
     private E data;
@@ -48,9 +66,10 @@ public class MyLinkedList<E>{
 
   public static void main(String[] args) {
     MyLinkedList<Integer> list = new MyLinkedList<>();
-    System.out.println(list.length);
-    System.out.println(list.start);
-    System.out.println(list.end);
+    //System.out.println(list.length);
+    //System.out.println(list.start);
+    //System.out.println(list.end);
+    System.out.println(list);
   }
 
 
